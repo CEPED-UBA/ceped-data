1412d094be6ec4e691e7e877041cdbd95f033957
unknown <guidowe45@gmail.com>
2022-07-12 09:06:12 -0300

    actualizo salarios

--- crudo/scripts/Salarios CCNN.R
+++ crudo/scripts/Salarios CCNN.R
@@ -2,42 +2,77 @@
 library(openxlsx)
 library(tidyverse)
 library(stringr)
+# Codigos ISO####
+options(scipen = 999)
+iso_codes_spa <- read.xlsx("crudo/datos/isocodes.xlsx")
+iso_codes_spa <- iso_codes_spa %>% 
+  select(iso3c,nombre.pais = pais)
 
-# Lectura de Datos####
-## CEPED-CCNN####
-Paises <- read.xlsx("crudo/datos/Prod y Salarios.xlsx",
-                    sheet = "Paises_Diccionario") %>% 
-  select(nombre.pais,iso3c)
+iso_codes_eng <- read.xlsx("crudo/datos/isocodes.xlsx",
+                           sheet = "INGLES")
 
 
-Salarios_UMN <- read.xlsx("crudo/datos/Prod y Salarios.xlsx",
+# Series####
+## Salarios e IPC ####
+Salarios_UMN <- read.xlsx("crudo/datos/comparación internacional - salarios promedio PPP.xlsx",
                           sheet = "salario nominal - UMN"
-                          ) %>%
+) %>%
   rename(ANO4 = X1)%>%
   pivot_longer(cols = 2:ncol(.),
-               names_to = "nombre.pais",
-               values_to = "Salario.UMN") %>%
-  mutate(nombre.pais = str_replace_all(nombre.pais,"[[:punct:] ]+",replacement = " "))
- 
-IPC_2005 <- read.xlsx("crudo/datos/Prod y Salarios.xlsx",
-                          sheet = "IPC (2005)") %>%
-  rename(ANO4 = X1)%>%
-  pivot_longer(cols = 2:ncol(.),
-               names_to = "nombre.pais",
-               values_to = "IPC_2005") %>%
-  mutate(nombre.pais = str_replace_all(nombre.pais,"[[:punct:] ]+",replacement = " "))
+               names_to = "iso3c",
+               values_to = "Salario.UMN")
 
-## TCN-WB####
+IPC_2005 <- read.xlsx("crudo/datos/comparación internacional - salarios promedio PPP.xlsx",
+                      sheet = "IPC (2005 o 2010)") %>%
+  rename(ANO4 = X1)%>%
+  pivot_longer(cols = 2:ncol(.),
+               names_to = "iso3c",
+               values_to = "IPC_2005")
+
+IPC_WB <- readRDS(file = "crudo/datos/IPC_WORLD_BANK.RDS") %>% 
+  select(iso3c,ANO4 = date,IPC_2010_WB = FP.CPI.TOTL)
+
+IPC_corregido <- IPC_2005 %>%
+  left_join(IPC_WB) %>% 
+  group_by(iso3c) %>% 
+  mutate(IPC_2005_corregido = 
+           case_when(all(is.na(IPC_2005)) ~ IPC_2010_WB,
+                     TRUE ~ IPC_2005)) %>% 
+  group_by(iso3c) %>% 
+  mutate(IPC_2005 = 100*IPC_2005_corregido/
+           IPC_2005_corregido[ANO4 == 2005]) %>% 
+  select(iso3c,ANO4,IPC_2005)
+
+## Tipo de cambio####
 Euro_area <- read.xlsx("crudo/datos/comparación internacional - salarios promedio PPP.xlsx",
                        sheet = "Euro Area")
-series_tcn <- readRDS("crudo/datos/series_tcn.RDS")
+tc <- readRDS("crudo/datos/series_tcn.RDS")
+tcn_ocde <- readxl::read_excel("crudo/datos/TCN_OCDE_1955-2013.xlsx")
+tcn_cepal <- readxl::read_excel("crudo/datos/TC Am Lat_CEPAL.xlsx") 
+###OCDE####
+tcn_ocde <- tcn_ocde %>% 
+  mutate_at(.vars = 3:ncol(.),.funs = as.numeric) %>% 
+  pivot_longer(cols = 3:ncol(.),names_to = "date",values_to = "tcn_ocde") %>% 
+  mutate(date = as.double(date))
 
-series_tcn<- series_tcn %>%
+###CEPAL####
+tcn_cepal <- tcn_cepal %>%   #mutate_at(.vars = 3:ncol(.),.funs = as.numeric) %>% 
+  pivot_longer(cols = 3:ncol(.),names_to = "date",values_to = "tcn_cepal") %>% 
+  mutate(date = as.double(date))
+
+
+# Corrijo países en Euros
+series_tcn_corregidas<- tc %>%
   group_by(date) %>%
-  mutate(TCN = case_when(
+  mutate(PA.NUS.FCRF = case_when(
     iso3c %in% Euro_area$`Alpha-3` ~ PA.NUS.FCRF[iso3c == "EMU"],
     TRUE ~ PA.NUS.FCRF)) %>% 
-  select(ANO4 = date,iso3c,TCN)
+  left_join(tcn_ocde) %>% 
+  left_join(tcn_cepal) %>% 
+  mutate(valor = if_else(is.na(PA.NUS.FCRF),tcn_ocde,PA.NUS.FCRF),
+         TCN = if_else(is.na(valor),tcn_cepal,valor)) %>% 
+  rename(ANO4 = date) %>% 
+  select(ANO4,iso3c,TCN)
 
 ## PPA####
 PPA_WB <- read.csv("crudo/datos/All_PPA_WB_2017.csv") %>%
@@ -59,12 +94,11 @@ names(PPA_2017_Cpriv_Cefect)[2] <- "PPA_2017_c_actual"
 names(PPA_2017_Cpriv_Cefect)[3] <- "PPA_2017_c_privado"
 
 
-PPA_extrapolados <- IPC_2005 %>% 
-  left_join(Paises) %>% 
+PPA_extrapolados <- IPC_corregido %>% 
   left_join(PPA_2017_Cpriv_Cefect) %>% 
   group_by(ANO4) %>% 
   mutate(IPC_2005_USA = IPC_2005[iso3c == "USA"]) %>% 
-  group_by(nombre.pais) %>%
+  group_by(iso3c) %>%
   mutate(PPA_c_actual_serie =
            case_when(
              ANO4 == 2017 ~ PPA_2017_c_actual,
@@ -87,11 +121,16 @@ PPA_series<- PPA_extrapolados %>%
 
 
 # Procesamiento ####
-base <- Paises %>% 
+paises_trabajados<- Salarios_UMN %>% 
+  select(iso3c) %>% 
+  unique()
+
+base <- paises_trabajados %>% 
+  left_join(iso_codes_spa) %>% 
   left_join(Salarios_UMN) %>% 
-  left_join(IPC_2005) %>% 
+  left_join(IPC_corregido) %>% 
   left_join(PPA_series) %>% 
-  left_join(series_tcn) %>% 
+  left_join(series_tcn_corregidas) %>% 
   mutate(salario_real_2005 = Salario.UMN/IPC_2005*100,
          salario_dolares = Salario.UMN/TCN,
          salario_ppa_c_priv_corr = Salario.UMN/PPA_c_priv_serie,
@@ -110,4 +149,4 @@ base_export <- base %>%
                names_to = "cod.variable",values_to = "valor") %>% 
   filter(!is.na(valor))
 
-saveRDS(base_export,file = "shiny/data/salarios.RDS")
+saveRDS(base_export,file = "opcion-modulos-paneles/www/data/salarios.RDS")
