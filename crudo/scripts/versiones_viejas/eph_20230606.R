51cb939da3feb9c9eb5e703365aae056b5e41e69
marajadesantelmo <50368116+marajadesantelmo@users.noreply.github.com>
2023-06-07 10:56:54 -0300

    eph_update

--- crudo/scripts/versiones_viejas/eph_20230606.R
+++ crudo/scripts/versiones_viejas/eph_20230606.R
@@ -43,6 +43,38 @@ base_puntual <- base_puntual %>%
 
 remove(base_mayo, base_octubre, base_puntual1, base_puntual2)
 
+# EPH Continua - Total aglomerados ####
+
+## Versión vieja ####
+# base_1er <- read_excel("crudo/datos/03. EPH_Total Aglomerados_empleo_Total.xlsx", 
+#                         sheet = "1°trim - 28 Aglo", skip = 1)
+# base_1er <- base_1er[3:21, 1:13]
+# 
+# base_2do <- read_excel("crudo/datos/03. EPH_Total Aglomerados_empleo_Total.xlsx", 
+#                            sheet = "2°trim - 28 Aglo", skip = 1)
+# base_2do <- base_2do[3:21, 1:13]
+# 
+# base_3er <- read_excel("crudo/datos/03. EPH_Total Aglomerados_empleo_Total.xlsx", 
+#                        sheet = "3°trim - 28 Aglo", skip = 1)
+# base_3er <- base_3er[3:21, 1:13]
+# 
+# base_4to <- read_excel("crudo/datos/03. EPH_Total Aglomerados_empleo_Total.xlsx", 
+#                        sheet = "4°trim - 28 Aglo", skip = 1)
+# base_4to <- base_4to[3:21, 1:13]
+# 
+# base_continua <- bind_rows(base_1er, base_2do, base_3er, base_4to)
+# 
+# colnames(base_continua) <- c("ANO4", "trimestre", "Tasa de actividad (EPH continua)", "Tasa de empleo (EPH continua)", "Tasa de empleo pleno (EPH continua)",
+#                             "Tasa de subocupación (EPH continua)", "Tasa de desocupación (EPH continua)","Patrón (EPH continua)",
+#                             "Cuenta Propia (EPH continua)", "Asalariado (EPH continua)", "TFSS (EPH continua)", "Protegido (EPH continua)", "Precario (EPH continua)" )
+# 
+# base_continua[, 3:ncol(base_continua)] <- sapply(base_continua[, 3:ncol(base_continua)], as.numeric)
+# 
+# base_continua <- base_continua %>% 
+#   mutate(ANO4.trim= paste0(ANO4, ".", trimestre)) %>% 
+#   select(-trimestre)
+
+## Versión Nueva ####
 ### Descarga de bases con paquete eph | MODIFICAR ACA PARA ACTUALIZAR ####
 
 # variables <- c("ANO4", "TRIMESTRE", "AGLOMERADO", "ESTADO", "INTENSI", "CH06", "PP07H",
@@ -70,9 +102,8 @@ base.eph <- readRDS("crudo/datos/eph_descargada.RDS")
 
 base.eph$CAT_OCUP[is.na(base.eph$CAT_OCUP)] <- 0              #Corrección porque CAT_OCUP tiene NAs para 2017
 
-caes <- eph::caes # Defino tabla de CAES porque aveces el paquete eph no lo encuentra
-
 base.eph <- base.eph %>%
+  #  filter(AGLOMERADO %in% 32:33) %>% 
   mutate(
     ESTADO = case_when(
       PJ1_1==1 & ESTADO==1 ~ 2,                              # planes jjhd van como desocupados
@@ -188,10 +219,10 @@ remove(base_puntual, base_continua, tabla)
 # EPH Puntual GBA ####
 
 base_gba_mayo <- read_excel("crudo/datos/01. EPH_GBA_empleo_Total.xlsx", 
-                            sheet = "Mayo - GBA", skip = 1)
+                                       sheet = "Mayo - GBA", skip = 1)
 
 base_gba_octubre <- read_excel("crudo/datos/01. EPH_GBA_empleo_Total.xlsx", 
-                               sheet = "Oct - GBA", skip = 1)
+                           sheet = "Oct - GBA", skip = 1)
 
 base_gba_puntual1 <- base_gba_mayo[3:32, 1:13]
 
@@ -200,8 +231,8 @@ base_gba_puntual2 <- base_gba_octubre[3:31, 1:13]
 base_gba_puntual <- bind_rows(base_gba_puntual1, base_gba_puntual2)
 
 colnames(base_gba_puntual) <- c("ANO4", "onda", "Tasa de actividad (EPH puntual)", "Tasa de empleo (EPH puntual)", "Tasa de empleo pleno (EPH puntual)",
-                                "Tasa de subocupación (EPH puntual)", "Tasa de desocupación (EPH puntual)","Patrón (EPH puntual)",
-                                "Cuenta Propia (EPH puntual)", "Asalariado (EPH puntual)", "TFSS (EPH puntual)", "Protegido (EPH puntual)", "Precario (EPH puntual)" )
+                    "Tasa de subocupación (EPH puntual)", "Tasa de desocupación (EPH puntual)","Patrón (EPH puntual)",
+                    "Cuenta Propia (EPH puntual)", "Asalariado (EPH puntual)", "TFSS (EPH puntual)", "Protegido (EPH puntual)", "Precario (EPH puntual)" )
 
 base_gba_puntual[,c(1, 3:ncol(base_gba_puntual))] <- sapply(base_gba_puntual[, c(1, 3:ncol(base_gba_puntual))], as.numeric)
 
@@ -212,7 +243,7 @@ base_gba_puntual <- base_gba_puntual %>%
   gather(., key="cod.variable", value="valor", 3:ncol(.)) %>% 
   mutate(nombre.pais="Argentina", 
          iso3c="ARG", 
-         aglomerados="gba",
+         aglomerados="total_aglos",
          modulo= case_when(
            cod.variable %in%  c("Tasa de actividad (EPH continua)", "Tasa de empleo (EPH continua)", "Tasa de empleo pleno (EPH continua)",
                                 "Tasa de desocupación (EPH continua)", "Tasa de subocupación (EPH continua)", "Tasa de actividad (EPH puntual)", 
@@ -228,6 +259,51 @@ base_gba_puntual <- base_gba_puntual %>%
 remove(base_gba_mayo, base_gba_octubre, base_gba_puntual1, base_gba_puntual2)
 
 # EPH Continua GBA ####
+##Version vieja ####
+# # base_gba_1er <- read_excel("crudo/datos/01. EPH_GBA_empleo_Total.xlsx", 
+# #                        sheet = "1°trim - GBA", skip = 1)
+# # base_gba_1er <- base_gba_1er[4:nrow(base_gba_1er), 1:13]
+# # 
+# # base_gba_2do <- read_excel("crudo/datos/01. EPH_GBA_empleo_Total.xlsx", 
+# #                        sheet = "2°trim - GBA", skip = 1)
+# # base_gba_2do <- base_gba_2do[4:nrow(base_gba_2do), 1:13]
+# # 
+# # base_gba_3er <- read_excel("crudo/datos/01. EPH_GBA_empleo_Total.xlsx", 
+# #                        sheet = "3°trim - GBA", skip = 1)
+# # base_gba_3er <- base_gba_3er[4:nrow(base_gba_3er), 1:13]
+# # 
+# # base_gba_4to <- read_excel("crudo/datos/01. EPH_GBA_empleo_Total.xlsx", 
+# #                        sheet = "4°trim - GBA", skip = 1)
+# # base_gba_4to <- base_gba_4to[4:nrow(base_gba_4to), 1:13]
+# # 
+# # base_gba_continua <- bind_rows(base_gba_1er, base_gba_2do, base_gba_3er, base_gba_4to)
+# # 
+# # colnames(base_gba_continua) <- c("ANO4", "trimestre", "Tasa de actividad (EPH continua)", "Tasa de empleo (EPH continua)", "Tasa de empleo pleno (EPH continua)",
+# #                              "Tasa de subocupación (EPH continua)", "Tasa de desocupación (EPH continua)","Patrón (EPH continua)",
+# #                              "Cuenta Propia (EPH continua)", "Asalariado (EPH continua)", "TFSS (EPH continua)", "Protegido (EPH continua)", "Precario (EPH continua)" )
+# # 
+# # base_gba_continua[, c(1, 3:ncol(base_gba_continua))] <- sapply(base_gba_continua[, c(1, 3:ncol(base_gba_continua))], as.numeric)
+# # 
+# # base_gba_continua <- base_gba_continua %>% 
+# #   mutate(ANO4.trim= paste0(ANO4, ".", trimestre)) %>% 
+# #   select(-trimestre)
+# 
+# base_gba_continua <- readRDS("www/data/eph_gba.rds") %>% 
+#   select(c("ANO4", "TRIMESTRE", "Tasa de actividad (EPH continua)", "Tasa de empleo (EPH continua)", "Tasa de empleo pleno (EPH continua)",
+#            "Tasa de subocupación (EPH continua)", "Tasa de desocupación (EPH continua)","Patrón (EPH continua)",
+#            "Cuenta Propia (EPH continua)", "Asalariado (EPH continua)", "TFSS (EPH continua)", 
+#            "Protegido (EPH continua)", "Precario (EPH continua)" )) %>% 
+#   mutate(TRIMESTRE2= case_when(                                         #Hay que quitarle etiquetas (ver si mejor saca etiquetas cuando se procesa el crudo)
+#     TRIMESTRE==1 ~ 1, 
+#     TRIMESTRE==2 ~ 2, 
+#     TRIMESTRE==3 ~ 3, 
+#     TRIMESTRE==4 ~ 4), 
+#     ANO4=remove_var_label(ANO4)) %>% 
+#   mutate(ANO4.trim= paste0(ANO4, ".", TRIMESTRE2)) %>% 
+#   select(-c("TRIMESTRE", "TRIMESTRE2")) %>%
+#   mutate(across(where(is.numeric), ~ .x * 100))
+
+##Version nueva ####
 
 tabla <- base.eph %>% 
   filter(AGLOMERADO %in% 32:33) %>%    #Filtro para GBA
@@ -275,7 +351,7 @@ base_gba_continua <- tabla %>%
   gather(., key="cod.variable", value="valor", 3:ncol(.)) %>% 
   mutate(nombre.pais="Argentina", 
          iso3c="ARG", 
-         aglomerados="gba",
+         aglomerados="total_aglos",
          ANO4=as.numeric(ANO4), 
          valor=valor*100,
          modulo= case_when(
@@ -303,7 +379,72 @@ eph_gba <- bind_rows(base_gba_puntual, base_gba_continua)
 remove(base_gba_puntual, base_gba_continua)
 
 eph <- bind_rows(eph_total_aglos, eph_gba)
-
-
 saveRDS(eph, file = "www/data/eph.RDS")
+
+
+# Categorias ocupacionales POK | 28 aglomerados 2003-2021 ####
+## Version vieja (Ahora se calculan por paquete eph)
+# base <- read_excel("crudo/datos/Datos Mercado de Trabajo - CEPED (bases).xls",
+#                    sheet = "28 trim - cat pok est")
+# 
+# base2 <- base[7:17,]
+# 
+# base2 <- base2 %>%
+#   rename(index = 1)
+# 
+# #Traspongo y pongo nombre de variables
+# base2 <- data.frame( t(base2[,-1]) )
+# colnames(base2) <- base$index
+# 
+# #Genero vector años
+# ANO4 <- c()
+# for (i in 2003:2021){
+#   ANO4 <- c(ANO4, rep(i, 4) )
+# }
+# 
+# base2[1] <- ANO4
+# 
+# #Genero vector trimestre
+# trimestre <- c()
+# for (i in 2003:2021){
+#   trimestre <- c(trimestre, seq(1, 4) )
+# }
+# 
+# base2[2] <- trimestre
+# 
+# colnames(base2) <- c("ANO4", "trimestre",
+#                      "Patrón (s/planes)",
+#                      "Cuenta Propia (s/planes)",
+#                      "Asalariados públicos (s/planes)",
+#                      "Asalariados privados protegidos (s/ serv dom ni planes)",
+#                      "Asalariados privados precarios (s/ serv dom ni planes)",
+#                      "Serv dom (s/planes)",
+#                      "Trabajador familiar",
+#                      "Planes JJHD",
+#                      "Desconocidos"
+# )
+# 
+# base2[] <- lapply(base2, function(x) as.numeric(as.character(x)))
+# 
+# rownames(base2) <- NULL
+# 
+# base2 <- base2 %>%
+#   mutate(ANO4.trim= paste0(ANO4, ".", trimestre)) %>%
+#   select(-trimestre)
+# 
+# base3 <- gather(base2, key="cod.variable", value="valor", 2:10 )
+# 
+# base3 <- base3 %>%
+#   mutate(nombre.pais="Argentina",
+#          iso3c="ARG") %>%
+#   mutate(aglomerados="28_aglos",
+#          modulo="categoria_ocupacional_pok")
+# 
+# Junto pok con la otra base
+# 
+# 
+# eph <- bind_rows(eph, base3)
+
+
+
 
