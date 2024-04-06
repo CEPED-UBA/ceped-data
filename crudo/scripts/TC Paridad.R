1ae43c28770b74c90d96b47e708d05a6c80f994a
juancamilogutman <gutmanjuancamilo@gmail.com>
2024-04-06 18:55:27 -0300

    Agregado archivo de Excel crudo con datos de TC de paridad y script para procesarlo para R.

---
+++ crudo/scripts/TC Paridad.R
@@ -0,0 +1,27 @@
+# Cargamos librerías
+library(openxlsx)
+library(tidyverse)
+library(stringr)
+
+# Leemos el Excel
+TC_paridad <- read.xlsx("crudo/datos/Precio dolar_Argentina_v4.xlsx",
+                           sheet = "data_sistematizada_cd",
+                           startRow = 1,
+                           cols = c(3:14),
+                           skipEmptyRows = T)
+
+# Convertimos de formato ancho a largo
+TC_paridad <- TC_paridad %>%
+  pivot_longer(cols = 2:ncol(.),
+               names_to = "cod.variable",
+               values_to = "valor")
+
+# Agregamos columnas de país
+TC_paridad <- TC_paridad %>%
+  mutate(iso3c = "ARG",
+         nombre.pais = "Argentina"
+         ) %>% 
+  select(nombre.pais,iso3c,everything())
+
+# Guardamos en formato para R
+saveRDS(TC_paridad,file = "data/TC_Paridad.RDS")
